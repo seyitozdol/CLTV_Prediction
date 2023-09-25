d18680c367bf59fbe4cfb75fb2c961e3e0257ffa
seyitozdol <144552413+seyitozdol@users.noreply.github.com>
2023-09-25 21:03:33 +0200

    Update CLTV_Prediction.py

--- CLTV_Prediction.py
+++ CLTV_Prediction.py
@@ -37,6 +37,9 @@
 import datetime as dt
 import numpy as np
 import pandas as pd
+from lifetimes import BetaGeoFitter
+from lifetimes import GammaGammaFitter
+from lifetimes.plotting import plot_period_transactions
 
 pd.set_option('display.max_columns',1000)
 pd.set_option('display.width', 500)
@@ -46,6 +49,9 @@ pd.set_option('display.float_format',lambda x : '%.5f' % x)
 df_ = pd.read_csv('flo_data_20K.csv')
 df = df_.copy()
 
+#*****************************
+# Task 1: Data Prepration
+#*****************************
 
 def analyze_missing_values(df):
     na_cols = df.columns[df.isna().any()].tolist()
@@ -77,4 +83,133 @@ def sum_df(dataframe, head=6):
     print(analyze_missing_values(dataframe))
 
 sum_df(df)
+
+def outlier_thresholds(dataframe, variable):
+    '''
+        Sets the boundaries of outliers.
+    Parameters
+    ----------
+    dataframe :DataFrame
+    variable : int
+
+    Returns
+    -------
+    low_limit : The low limit of data
+    up_limit : The up limit of data
+    '''
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def replace_with_thresholds(dataframe, variable):
+    '''
+        The function that necessary to suppress outliers
+    Parameters
+    ----------
+    dataframe: DataFrame
+    variable : int
+
+    Returns
+    -------
+    '''
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = round(low_limit,0)
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = round(up_limit,0)
+
+
+columns = [i for i in df.columns if df[i].dtype != "O"]
+
+for col in columns:
+    replace_with_thresholds(df, col)
+
+#*********************************************
+# Task 2: Creation of the CLTV Data Structure
+#*********************************************
+
+df["tot_ord_num"] = df["order_num_total_ever_online"] + df["order_num_total_ever_offline"]
+
+df["tot_customer_value"] = df["customer_value_total_ever_offline"] + df["customer_value_total_ever_online"]
+
+date_columns = [i for i in df.columns if "date" in i.lower()]
+df[date_columns] = df[date_columns].apply(pd.to_datetime)
+
+df["last_order_date"].max()
+#  '2021-05-30'
+last_date = dt.datetime(2021,5,30)
+today_date = dt.datetime(2021, 6, 2)
+
+cltv_df = pd.DataFrame({"customer_id": df["master_id"],
+             "recency_cltv_weekly": ((df["last_order_date"] - df["first_order_date"]).dt.days)/7,
+             "T_weekly": ((today_date - df["first_order_date"]).astype('timedelta64[D]'))/7,
+             "frequency": df["tot_ord_num"],
+             "monetary_cltv_avg": df["tot_customer_value"] / df["tot_ord_num"]})
+
+
+#*********************************************************************************
+# Task 3: Establishment of BG/NBD and Gamma-Gamma Models and Calculation of CLTV
+#*********************************************************************************
+
+# STEP 1 : Fit the BG/NBD Model
+
+bgf = BetaGeoFitter(penalizer_coef=0.001)
+
+bgf.fit(cltv_df['frequency'],
+       cltv_df['recency_cltv_weekly'],
+       cltv_df['T_weekly'])
+
+cltv_df["exp_sales_3_month"] = bgf.conditional_expected_number_of_purchases_up_to_time(4*3,
+                                                        cltv_df['frequency'],
+                                                        cltv_df['recency_cltv_weekly'],
+                                                        cltv_df['T_weekly'])
+
+cltv_df["exp_sales_6_month"] = bgf.predict(4*6,
+                                       cltv_df['frequency'],
+                                       cltv_df['recency_cltv_weekly'],
+                                       cltv_df['T_weekly'])
+
+# STEP 2 : Fit the Gamma Gamma Model
+ggf = GammaGammaFitter(penalizer_coef=0.01)
+
+ggf.fit(cltv_df['frequency'], cltv_df['monetary_cltv_avg'])
+
+cltv_df["exp_average_profit"] = ggf.conditional_expected_average_profit(cltv_df['frequency'], cltv_df['monetary_cltv_avg'])
+
+cltv = ggf.customer_lifetime_value(bgf,
+                                       cltv_df['frequency'],
+                                       cltv_df['recency_cltv_weekly'],
+                                       cltv_df['T_weekly'],
+                                       cltv_df['monetary_cltv_avg'],
+                                       time=6,  # 6 aylık
+                                       freq="W",  # T'nin frekans bilgisi.
+                                       discount_rate=0.01)
+
+cltv_df["cltv"] = cltv
+cltv_df.sort_values("cltv",ascending=False)[:10]
+
+#*********************************************
+# Task 4: Segmentation Based on CLTV Value
+#*********************************************
+
+cltv_df["cltv_segment"] = pd.qcut(cltv_df["cltv"], 4, labels=["D", "C", "B", "A"])
+
+cltv_df.head(3)
+#                             customer_id  recency_cltv_weekly  T_weekly  frequency  monetary_cltv_avg  exp_sales_3_month  exp_sales_6_month  exp_average_profit      cltv cltv_segment
+# 0  cc294636-19f0-11eb-8d74-000d3a38a36f             17.00000  30.71429    5.00000          187.87400            0.83886            1.67772           193.60204 340.79797            A
+# 1  f431bd5a-ab7b-11e9-a2fc-000d3a38a36f            209.85714 225.00000   10.00000          138.09700            0.52953            1.05907           140.28486 155.88455            B
+# 2  69b69676-1a40-11ea-941b-000d3a38a36f             52.28571  79.00000    5.00000          117.06400            0.62216            1.24431           120.94931 157.90656            B
+
+cltv_df.groupby("cltv_segment").agg({"count","mean","sum"})
+#              recency_cltv_weekly                     T_weekly                    frequency                   monetary_cltv_avg                     exp_sales_3_month                  exp_sales_6_month                  exp_average_profit                          cltv
+#                             mean count          sum      mean count          sum      mean count         sum              mean count           sum              mean count        sum              mean count        sum               mean count           sum      mean count           sum
+# cltv_segment
+# D                      138.72425  4987 691817.85714 161.97141  4987 807751.42857   3.74093  4987 18656.00000          92.38507  4987  460724.32698           0.40452  4987 2017.35046           0.80904  4987 4034.70091           97.79566  4987  487706.94067  79.45112  4987  396222.75123
+# C                      100.80173  4986 502597.42857 120.79726  4986 602295.14286   4.35780  4986 21728.00000         126.95077  4986  632976.55825           0.49045  4986 2445.39334           0.98090  4986 4890.78668          133.23154  4986  664292.46104 131.79758  4986  657142.73509
+# B                       81.95705  4986 408637.85714 100.59805  4986 501581.85714   4.74007  4986 23634.00000         158.50378  4986  790299.84401           0.54990  4986 2741.77839           1.09979  4986 5483.55679          165.64402  4986  825901.06379 183.88841  4986  916867.59852
+# A                       59.56223  4986 296977.28571  75.08255  4986 374361.57143   4.98355  4986 24848.00000         218.70167  4986 1090446.52566           0.64156  4986 3198.82105           1.28312  4986 6397.64210          228.34154  4986 1138510.92410 292.31831  4986 1457499.10773
+
+
+
 
